80f19af8414771df7683881b20d5c950b2d88a44
Quang Tung <tungtq99@outlook.com.vn>
2021-06-11 08:42:51 +0700

    add build

---
+++ build/precache-manifest.36eec0754472bb940d0c2e4fa9415d8b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217d22e5982d0ddabda5496ac366be55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb64c9acf3bba2312622",
+    "url": "/static/css/main.9f9ae3a0.chunk.css"
+  },
+  {
+    "revision": "4dd066b174dee9f6aed8",
+    "url": "/static/js/2.067cddeb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.067cddeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb64c9acf3bba2312622",
+    "url": "/static/js/main.715d4b9d.chunk.js"
+  },
+  {
+    "revision": "af176f287c0c307df59b",
+    "url": "/static/js/runtime-main.cb91ee66.js"
+  },
+  {
+    "revision": "f68562f9ddd55723e713dfdbe396752a",
+    "url": "/static/media/Man_Construction_Worker.f68562f9.png"
+  },
+  {
+    "revision": "1066af7ad27531e339016153f5c24b3b",
+    "url": "/static/media/bgpc.1066af7a.jpg"
+  },
+  {
+    "revision": "85797fa79bf293c3bf479cf53dab9a91",
+    "url": "/static/media/female-singer_bowie.85797fa7.png"
+  },
+  {
+    "revision": "40fee9290d26c9ee6d995c7cbfafacb2",
+    "url": "/static/media/female-singer_prince.40fee929.png"
+  },
+  {
+    "revision": "3c07eaf21b55da80d2da93a87dc0567e",
+    "url": "/static/media/male-singer_bowie.3c07eaf2.png"
+  },
+  {
+    "revision": "8022fd28bb06aba2a3008886d1168d75",
+    "url": "/static/media/male-singer_prince.8022fd28.png"
+  },
+  {
+    "revision": "d4a675321fd0ce8191837ec910afab73",
+    "url": "/static/media/multiple-musical-notes.d4a67532.png"
+  },
+  {
+    "revision": "6ded9f253b6e01c14d835f8be29f2408",
+    "url": "/static/media/popular-genres.6ded9f25.jpeg"
+  }
+]);
